94e9f15822711bf657642070dc9614f4cecc0e1d
snehareddy62 <shdy13is@cmrit.ac.in>
2016-05-27 09:19:23 +0530

    completed

--- lab7/src/lab7.cpp
+++ lab7/src/lab7.cpp
@@ -5,118 +5,68 @@
 // Copyright   : Your copyright notice
 // Description : cosequential matching using files
 //============================================================================
-#include <iostream>
-#include <fstream>
-#define max 100
+#include<iostream>
+#include<string>
+#include<fstream>
+#include<vector>
 using namespace std;
-class coseq
-{
-	string name,buffer;
-	int count1,count2;
-	string a[max],b[max];
-public :
-	void load();
-	void sort();
-	void insert();
-	void read();
-	void match();
-};
-void coseq::read()
-{
-	int i,n;
-	cout<<"enter how many names to enter in list1"<<endl;
-	cin>>n;
-	for(i=0;i<n;i++)
+class namelist{
+	vector <string> names;
+public:
+	void load(string filename)
 	{
-		buffer.erase();
-		cout<<"Enter name";
-		cin>>name;
-		buffer+=name;
-	}
-}
-void coseq::insert()
-{
-	fstream fp;
-	read();
-	fp.open("list1.txt",ios::out|ios::app);
-	fp<<buffer<<endl;
-	fp.close();
-	read();
-	fp.open("list2.txt",ios::out|ios::app);
-	fp<<buffer<<endl;
-	fp.close();
-}
-void coseq::load()
-{
-	fstream fp;
-	count1 = 0;
-	fp.open("list1.txt",ios::in);
-	while(fp)
-	{
-		getline(fp,name);
-		a[count1++]=name;
-	}
-	fp.close();
-	count2 = 0;
-	fp.open("list2.txt",ios::in);
-	while(fp)
-	{
-		getline(fp,name);
-		b[count2++]=name;
-	}
-	fp.close();
-}
-void sorting(string arr[],int n)
-{
-	int i,j;
-	string temp;
-	for(i=0;i<n;i++)
-	{
-		for(j=i+1;j<n;j++)
+		string s;
+		ifstream f1(filename.c_str());
+		while(!f1.eof())
 		{
-			if(arr[i]>arr[j])
+			getline(f1,s);
+			names.push_back(s);
+		}
+		int i,j;
+		string temp;
+		int n=names.size();
+		for(i=0;i<n-1;i++)
+		{
+			for(j=i+1;j<n;j++)
 			{
-				temp=arr[i];
-				arr[i]=arr[j];
-				arr[j]=temp;
+				if(names[i]>names[j])
+				{
+					temp=names[i];
+					names[i]=names[j];
+					names[j]=temp;
+
+				}
 			}
 		}
+		cout<<endl<<"Names in file";
+		for(i=0;i<n;i++)
+			cout<<names[i]<<endl;
 	}
-}
-void coseq::sort()
-{
-	int i;
-	sorting(a,count1);
-	cout<<"\nThe Sorted Contents of List 1 : \n";
-	for(i=0;i<=count1;i++)
-			cout<<a[i]<<"\n";
-	sorting(b,count2);
-	cout<<"\nThe Sorted Contents of List 2 : \n";
-	for(i=0;i<=count2;i++)
-		cout<<b[i]<<"\n";
-}
-void coseq::match()
-{
-	int i=0,j=0;
-	while(i<=count1 && j<=count2)
+	void cosq(namelist &l2)
 	{
-		if(a[i]==b[j])
+		int i=0,j=0;
+		cout<<endl<<"Common names";
+		while(i<names.size() && j<l2.names.size())
 		{
-			cout<<a[i]<<endl;
-			i++;
-			j++;
+			if(names[i]==l2.names[j])
+			{
+				cout<<names[i]<<endl;
+				i++;
+				j++;
+			}
+			else if(names[i]<l2.names[j])
+				i++;
+			else
+				j++;
 		}
-		if(a[i]<b[j])i++;
-		if(a[i]>b[j])j++;
 	}
-}
+};
 int main()
 {
-	coseq c;
-	c.insert();
-	c.load();
-	c.sort();
-	cout<<"\nCommon Names in Both Lists Are : \n";
-	c.match();
-	return 0;
+	namelist n1;
+	n1.load("list1.txt");
+	namelist n2;
+	n2.load("list2.txt");
+	n1.cosq(n2);
 }
+
